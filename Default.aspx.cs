dceccd13553fd67286ecd8c0eac84d4d89aa45b2
Erik Rogers <erikrogers88@gmail.com>
2018-05-05 22:17:18 -0400

    Fixed Directory structure. Added .aspx support.

---
+++ Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.newUserTitle.Text = "Eriks Banking Site";
+        this.header.InnerHtml = "New User Registration";
+        this.newUserFormIntroText.InnerHtml = "Please enter your name.";
+        this.afterNewUserFormDiv.InnerHtml = "This div appears after the form.";
+    }
+}
