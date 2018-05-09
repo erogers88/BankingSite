d2f3600dafd0f8922b0d280c89bbb06237e86d73
Erik Rogers <erikrogers88@gmail.com>
2018-05-08 20:47:23 -0400

    Create a form and connect to SQL database. Able to submit information to create new user.

---
+++ App_Code/SQLConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Linq.Expressions;
+
+namespace Database
+{
+    public static class DB
+    {
+        public static readonly string connectionString = @"Data Source = (local)\SQLTESTSERVER; Initial Catalog = Bank; Integrated Security = True";
+    }
+}
