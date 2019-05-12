7b3191ef829b4ed048bf64a74bdea42ef5f213af
Erik Rogers <erogers88@gmail.com>
2019-05-11 22:34:02 -0400

    Converted to MVC Razor V3. Got rid of code behind files. Added validation on email and username input.

---
+++ Controllers/Default.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlankSolution.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Default()
+        {
+            ViewBag.Message = "Test ViewBag Message";
+            return View();
+        }
+    }
+}
